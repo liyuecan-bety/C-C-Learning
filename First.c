162ab5b23c4b64d0422fd5145ee1d369b853acd3
liyuecan-bety <liyuecan960@gmail.com>
2026-07-23 17:12:08 +0800

    second

--- First.c
+++ First.c
@@ -4,5 +4,10 @@ int main()
     printf("hello");
     int a = 10;
     printf("%d", a);
+    int b[] = {1, 2, 3, 4, 5};
+    for(int i = 0; i < 5; i++)
+    {
+        printf("%d", b[i]);
+    }
     return 0;
 }
